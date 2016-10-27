0172a351a4385f94b8c62b4259d043d99ec7c87e
bjw4ph <bjw4ph@virginia.edu>
2016-10-27 00:30:02 -0400

    fixes to run on EC2 instance

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/tuber');
+mongoose.connect(database.url);
 app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
