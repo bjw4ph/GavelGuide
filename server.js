9c23cc76125f10d145bf41ce8c2a9cc5a1bc7b59
bjw4ph <bjw4ph@virginia.edu>
2016-11-19 18:48:14 -0500

    fix the fields returned in json for the pairings

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/getTeams', function(req,res){
 })
 
 app.get('/getPairing/:pairingId', function(req,res){
-	var query = Pairing.find({'_id': req.params.pairingId}).populate('team1ID').populate('team2ID').populate('locationID').populate('judgeID');
+	var query = Pairing.find({'_id': req.params.pairingId}).populate('team1ID', 'name').populate('team2ID', 'name').populate('locationID').populate('judgeID');
 	query.exec(function(error,results){
 		res.json({results: results});
 	});
